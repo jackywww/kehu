817ff53f565fce0aaef2eb69cb4a3eaa9436b5bf
Jacky <503354703@qq.com>
2020-03-04 17:21:59 +0800

    bak

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/my/repo
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.5.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.2.0 // indirect
 	github.com/go-redis/redis/v7 v7.2.0 // indirect
 	github.com/go-xorm/xorm v0.7.9 // indirect
+	github.com/jinzhu/configor v1.1.1 // indirect
+	github.com/micro/go-micro v1.18.0 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
   "github.com/gin-gonic/gin"
   "github.com/go-playground/validator/v10"
   "github.com/satori/go.uuid"
+  "github.com/dgrijalva/jwt-go"
+  //"github.com/dgrijalva/jwt-go/request"
+  //"github.com/micro/go-micro/config"
   _ "github.com/lib/pq"
   "github.com/go-xorm/xorm"
+  "github.com/jinzhu/configor"
   "encoding/json"
 	"io/ioutil"
 	"fmt"
 	"net/http"
   "crypto/md5"
   "io"
+  "time"
+  "strings"
+  "regexp"
 )
 
 var validate *validator.Validate
@@ -27,9 +34,37 @@ const (
 
 type Password struct {
   Id string `json:"id" sql:",type:uuid"`
-  Mobile string `json:"moblie"`
-  Password string `json:"-"`
+  Mobile string `json:"mobile" required:"true" validate:"required"`
+  Password string `json:"password,omitempty" validate:"required"`
 }
+
+type Login struct {
+  Mobile string `json:"mobile" validate:"required"`
+  Password string `json:"password" validate:"required"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+var Config = struct {
+  Db struct {
+    Engine string `default:"postgres"`
+    Host  string `default:"localhost"`
+    Name     string
+    User     string `default:"root"`
+    Password string `required:"true"`
+    Port     uint   `default:"5432"`
+    Sslmode string `default:"disable"`
+  }
+  Rsa struct {
+    PublicKeyPath string
+    PrivateKeyPath string
+    Iss string `default:"Jacky"`
+    Exp int `default:"1"`
+    SigningMethod string `default:"RS512"`
+  }
+}{}
 
 func errHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -88,9 +123,115 @@ func md5String(str string) string {
     return md5str
 }
 
+func getTokenString(id string)(tokenString string) {
+  if Config.Rsa.PrivateKeyPath == "" {
+    panic("Please config private key path!")
+  }
+
+  privateKey, err := ioutil.ReadFile(Config.Rsa.PrivateKeyPath)
+  if err != nil {
+      panic(err)
+  }
+
+  claims := make(jwt.MapClaims)
+  claims["exp"] = time.Now().Add(time.Hour * time.Duration(Config.Rsa.Exp)).Unix()
+  claims["iat"] = time.Now().Unix()
+  claims["iss"] = Config.Rsa.Iss
+  claims["id"] = id
+
+  method := jwt.GetSigningMethod(Config.Rsa.SigningMethod)
+  token := jwt.NewWithClaims(method, claims)
+
+  var privateKeyString interface{}
+  if strings.Index(Config.Rsa.SigningMethod, "HS") > -1 {
+    privateKeyString = privateKey
+  } else {
+    privateKeyString, err = jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+    if err != nil {
+      panic(err)
+    }
+  }
+
+  tokenString, err = token.SignedString(privateKeyString)
+  if err != nil {
+    panic(err)
+  }
+
+  return tokenString
+}
+
+func parseAndVertifyToken(tokenString string)(token *jwt.Token, e error){
+  publicKey, err := ioutil.ReadFile(Config.Rsa.PublicKeyPath)
+  if err != nil {
+    panic(err)
+  }
+
+  var publicKeyString interface{}
+  if strings.Index(Config.Rsa.SigningMethod, "HS") > -1 {
+    publicKeyString = publicKey
+  } else {
+    publicKeyString, err = jwt.ParseRSAPublicKeyFromPEM(publicKey)
+    if err != nil {
+      panic(err)
+    }
+  }
+
+  token, err = jwt.ParseWithClaims(tokenString, jwt.MapClaims{}, func(token *jwt.Token) (i interface{}, e error) {
+      return publicKeyString, nil
+  })
+  if err != nil {
+    return nil, err
+  }
+
+  return token, err
+}
+
+//mobile verify
+func VerifyMobileFormat(mobileNum string) bool {
+	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+
+	reg := regexp.MustCompile(regular)
+	return reg.MatchString(mobileNum)
+}
+
+func validateLoginStruct(item Login) {
+  err := validate.Struct(item)
+
+	if err != nil {
+    //errInfo(err)
+    if _, ok := err.(*validator.InvalidValidationError); ok {
+      panic(err)
+      return
+    }
+
+    for _, err := range err.(validator.ValidationErrors) {
+      panic(err.Field() + " must be " + err.ActualTag() + " " + err.Param())
+    }
+    return
+	}
+}
+
+func validatePasswordStruct(item Password) {
+  err := validate.Struct(item)
+
+	if err != nil {
+    //errInfo(err)
+    if _, ok := err.(*validator.InvalidValidationError); ok {
+      panic(err)
+      return
+    }
+
+    for _, err := range err.(validator.ValidationErrors) {
+      panic(err.Field() + " must be " + err.ActualTag() + " " + err.Param())
+    }
+    return
+	}
+}
+
 func main() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",host,port,user,password,dbName)
-  engine, _ = xorm.NewEngine("postgres", psqlInfo)
+  configor.Load(&Config, "config.yml")
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",Config.Db.Host, Config.Db.Port, Config.Db.User, Config.Db.Password, Config.Db.Name, Config.Db.Sslmode)
+  engine, _ = xorm.NewEngine(Config.Db.Engine, psqlInfo)
 
   //gin.SetMode(gin.ReleaseMode)
   router := gin.Default()
@@ -99,22 +240,32 @@ func main() {
   validate = validator.New()
 
 	router.POST("/customer", func(c *gin.Context) {
-    //body, _ := ioutil.ReadAll(c.Request.Body)
-    var pass Password
+    body, _ := ioutil.ReadAll(c.Request.Body)
+    var bodyCustomer, pass Password
+		if errIf := json.Unmarshal(body, &bodyCustomer); errIf != nil {
+      panic(errIf)
+		}
+
+    validatePasswordStruct(bodyCustomer)
+    if VerifyMobileFormat(bodyCustomer.Mobile) == false {
+      panic("Mobile format error!")
+    }
+
     u1 := uuid.NewV4()
 
     pass.Id = u1.String()
-    pass.Mobile = "111122"
-    pass.Password = md5String("abc")
+    pass.Mobile = bodyCustomer.Mobile
+    pass.Password = md5String(bodyCustomer.Password)
 
-		// var bodyCustomer []Customer
-		// if errIf := json.Unmarshal(body, &bodyCustomer); errIf != nil {
-    //   panic(errIf)
-		// }
     row, err := engine.Insert(&pass)
+    if err != nil {
+      panic(err)
+    }
+    if row == 0 {
+      panic("Insert Fail!")
+    }
 
-    fmt.Println(row, err)
-
+    pass.Password = ""
     c.Set("data", pass)
 	})
 
@@ -148,6 +299,41 @@ func main() {
     engine.SQL("select * from password").Find(&password)
 
     c.Set("data", password)
+	})
+
+  router.POST("/customer/login", func(c *gin.Context) {
+    body, _ := ioutil.ReadAll(c.Request.Body)
+
+		var bodyCustomer Login
+		if errIf := json.Unmarshal(body, &bodyCustomer); errIf != nil {
+      panic(errIf)
+		}
+    validateLoginStruct(bodyCustomer)
+    if VerifyMobileFormat(bodyCustomer.Mobile) == false {
+      panic("Mobile format error!")
+    }
+
+    cols := []string{"id", "mobile", "password"}
+    var user Password
+    var valuesMap = make(map[string]string)
+    if has, _ := engine.Table(&user).Where("mobile = ?", bodyCustomer.Mobile).Cols(cols...).Get(&valuesMap); has {
+      if valuesMap["password"] != md5String(bodyCustomer.Password) {
+        c.Set("data", gin.H{"token": nil})
+        return
+      }
+    }
+
+    tokenString := getTokenString(valuesMap["id"])
+    // token, err := parseAndVertifyToken(tokenString)
+    // if err != nil {
+    //   panic(err)
+    // }
+    //
+    // aaaaa := token.Claims.(jwt.MapClaims)
+    // aaaaa["token"] = tokenString
+
+    c.SetCookie("token", tokenString, -1, "/", "localhost", false, true)
+    c.Set("data", gin.H{"token": tokenString})
 	})
 
   router.Run(":8080")
